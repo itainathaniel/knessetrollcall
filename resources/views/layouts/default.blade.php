c91c4bf28b37ea3d66dd9797796698bd4cd194e7
Itai Nathaniel <itainathaniel@gmail.com>
2015-06-20 14:48:34 +0300

    twitter cards

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="he">
     <head>
-        <title>{{ Lang::get('index.site.title') }} @yield('title', '')</title>
+        <title>{{ Lang::get('index.site.title') }} @yield('title')</title>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="See which Israeli Knesset Member is in the Knesset">
         <meta name="author" content="Itai Nathaniel">
+
+        @include('layouts.partials.twitter-cards')
 
         <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="/css/all.css">
