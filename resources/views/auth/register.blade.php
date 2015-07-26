668b8d6ee110ed69f30df93f0ea0ce91ceaffa82
Itai Nathaniel <itainathaniel@gmail.com>
2015-07-26 08:06:45 +0300

    reports fix view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itainathaniel
+ * Date: 7/25/15
+ * Time: 23:44
+ */
--- resources/views/emails/monthly.blade.php
+++ resources/views/emails/monthly.blade.php
@@ -3,6 +3,56 @@
 @section('content')
     <h1>{{ Lang::get('emails.monthly-report.title', ['date' => $dates_title]) }}</h1>
 
+    <h2>כמה מספרים</h2>
+
+    <table class="table table-striped">
+        <tbody>
+        <tr>
+            <td>דקות כוללות</td>
+            <td>{{ $minutes }}</td>
+        </tr>
+        <tr>
+            <td>שעות כוללות</td>
+            <td>{{ minutesToHours($minutes) }}</td>
+        </tr>
+        <tr>
+            <td>ממוצע דקות לח״כ</td>
+            <td>{{ $minutesPerKM }}</td>
+        </tr>
+        <tr>
+            <td>ממוצע שעות לח״כ</td>
+            <td>{{ minutesToHours($minutesPerKM) }}</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <h2>נוכחות לפי מפלגות</h2>
+
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>מפלגה</th>
+            <th>נכחו</th>
+            <th>סך הכל דקות</th>
+            <th>סך הכל שעות</th>
+            <th>ממוצע דקות</th>
+            <th>ממוצע שעות</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($parties as $party)
+            <tr>
+                <td>{{ $party['name'] }}</td>
+                <td>{{ $party['members'] }}</td>
+                <td>{{ $party['minutes'] }}</td>
+                <td>{{ minutesToHours($party['minutes']) }}</td>
+                <td>{{ round($party['minutes']/$party['members'],3) }}</td>
+                <td>{{ minutesToHours(round($party['minutes']/$party['members'])) }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
     <h2>{{ Lang::get('emails.monthly-report.absent', ['count' => count($absent)]) }}</h2>
 
     <table class="table table-striped">
