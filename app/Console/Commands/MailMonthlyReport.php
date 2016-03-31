8d4cf52cb6e9845beb86d8c6c77f386a4225ba62
Itai Nathaniel <itainathaniel@gmail.com>
2016-03-31 03:27:41 -0400

    Applied fixes from StyleCI

--- app/Console/Commands/MailMonthlyReport.php
+++ app/Console/Commands/MailMonthlyReport.php
@@ -2,57 +2,57 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Controllers\ReportsController;
 use App\User;
 use DateTime;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Mail;
-use App\Http\Controllers\ReportsController;
 
 class MailMonthlyReport extends Command
 {
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'krc:mail:monthly';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'krc:mail:monthly';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Email month report to me.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Email month report to me.';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function handle()
-	{
-		$start = new DateTime('first day of last month');
-		$end = new DateTime('last day of last month');
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $start = new DateTime('first day of last month');
+        $end = new DateTime('last day of last month');
 
-		$report = new ReportsController($start->getTimestamp(), $end->getTimestamp());
-		$report->setView('emails.monthly');
+        $report = new ReportsController($start->getTimestamp(), $end->getTimestamp());
+        $report->setView('emails.monthly');
 
-		$users = (new User)->monthlyRecipients();
-		foreach ($users as $user) {
-			Mail::send('emails.raw', ['content' => $report->emailContent()], function ($message) use ($user) {
-				$message->to($user->email)->subject(Lang::get('emails.monthly-report.subject'));
-			});
-		}
-	}
+        $users = (new User())->monthlyRecipients();
+        foreach ($users as $user) {
+            Mail::send('emails.raw', ['content' => $report->emailContent()], function ($message) use ($user) {
+                $message->to($user->email)->subject(Lang::get('emails.monthly-report.subject'));
+            });
+        }
+    }
 }
--- resources/lang/he/fullscreen.php
+++ resources/lang/he/fullscreen.php
@@ -2,13 +2,13 @@
 
 return [
     'tables' => [
-    	'thead' => [
-    		'title' => ':side: :in מתוך :out חברי כנסת',
-    		'side' => [
-    			0 => 'אופוזיציה',
-    			1 => 'קואליציה'
-    		],
-    	],
-    	'out_of' => ':inside מתוך :total',
+        'thead' => [
+            'title' => ':side: :in מתוך :out חברי כנסת',
+            'side'  => [
+                0 => 'אופוזיציה',
+                1 => 'קואליציה',
+            ],
+        ],
+        'out_of' => ':inside מתוך :total',
     ],
 ];
