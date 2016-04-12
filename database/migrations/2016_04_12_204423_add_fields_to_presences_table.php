88004c21a0e11558b60f6e67c044c8a66bc086be
Itai Nathaniel <itainathaniel@gmail.com>
2016-04-12 21:58:54 +0300

    migrate this

---
+++ database/migrations/2016_04_12_204423_add_fields_to_presences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToPresencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('presences', function (Blueprint $table) {
+            $table->integer('party_id');
+            $table->integer('is_coalition');
+            $table->integer('week_day');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('presences', function (Blueprint $table) {
+            $table->dropColumn('party_id');
+            $table->dropColumn('is_coalition');
+            $table->dropColumn('week_day');
+        });
+    }
+}
