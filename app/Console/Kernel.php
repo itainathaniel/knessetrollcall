7f31cef0cb4253e2ecc9428dea55e31e553c6c82
Itai Nathaniel <itainathaniel@gmail.com>
2016-04-13 10:57:20 +0300

    return to every 10 minutes after long night

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('krc:log')->everyFiveMinutes();
-        $schedule->command('krc:presence')->everyMinute(); // everyTenMinutes();
+        $schedule->command('krc:presence')->everyTenMinutes();
         $schedule->command('krc:mail:daily')->dailyAt('7:00');
         $schedule->command('krc:mail:weekly')->weekly()->sundays()->at('7:00');
         $schedule->command('krc:mail:monthly')->monthly()->at('7:00');
